130e862aa3c1ad53d18db8be96acb52357880e73
Miguel235711 <a01423629@itesm.mx>
2020-02-23 00:42:19 -0600

    file structure and basic component review

--- React-Web/react-complete-guide/src/App.js
+++ React-Web/react-complete-guide/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Person from './Person/Person';
+// importing React is very important to allow rendering, because behind the scenes the jsx uses React. methods
+// render is the method called by react to render
 import './App.css';
 
 class App extends Component {
   render() {
+    // the jsx way: which allows syntatic sugar by writing pseudo html, because some html keywords are not allowed, like class,etc.
     return (
+      //this code is jsx (a syntatic sugar), it works both in .js and jsx because it does not depend on the extension of the file
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person/>
+        <Person/>
+        <Person/>
       </div>
-    );
+      //it is a very good practice to wrap everything into a root element, e.g. <div className="App"> ... </div>
+    );//these parentheses is to avoid getting error messages
+    //the manual way
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi, I\'m a React App!!!'));
   }
 }
 
