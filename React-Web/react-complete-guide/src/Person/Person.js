4b816e92eac194a21233e1b9547f09f40a9b77a2
Miguel235711 <a01423629@itesm.mx>
2020-02-26 13:12:44 -0600

    CSS modules finished

--- React-Web/react-complete-guide/src/Person/Person.js
+++ React-Web/react-complete-guide/src/Person/Person.js
@@ -31,7 +31,7 @@ export default Radium(person);*/
 ///!!! Using styled components:
 
 
-import React from 'react';
+/*import React from 'react';
 //import './Person.css'; // Because of web pack that is used by React we can import the css file. 
 import styled from 'styled-components';
 //Web pack takes care of injecting the css
@@ -58,7 +58,7 @@ const person = (props) => { // props holds all the attributes passed in the html
             width: '450px'
         }
     };*/
-    return (
+    /*return (
         <StyledDiv>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
@@ -68,4 +68,35 @@ const person = (props) => { // props holds all the attributes passed in the html
     //props.children refers to the elements that are inside the html tag
 }
 
+export default person;*/
+
+///!!! Using CSS modules:
+
+
+import React from 'react';
+//import './Person.css'; // Because of web pack that is used by React we can import the css file. 
+import classes from './Person.css';
+//Web pack takes care of injecting the css
+
+//This is an stateless way
+const person = (props) => { // props holds all the attributes passed in the html tag
+    // media query inline definition
+    // IMPORTANT!:  to transform media query selectors and keyframe animations we need to wrap the application it with Style Root
+    //this is not working
+    /*const style={
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };*/
+    return (
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    );
+    //props.children refers to the elements that are inside the html tag
+}
+
 export default person;
+
